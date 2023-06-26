fe0b582ea31daeabc2af5f9db386236b21edc40d
scantea <71529653+scantea@users.noreply.github.com>
2023-06-26 13:27:10 -0700

    updated BrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
     ,
