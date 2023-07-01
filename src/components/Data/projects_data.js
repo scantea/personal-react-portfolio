9428c7c3514d9bb365d3e9648ae3036660ba444f
scantea <71529653+scantea@users.noreply.github.com>
2023-07-01 01:37:48 -0700

    portfolio page updates

--- src/components/Data/projects_data.js
+++ src/components/Data/projects_data.js
@@ -1,11 +1,12 @@
 import portfolio from "../../assets/ProjectImages/web-pic.png";
+import chatbot from "../../assets/ProjectImages/chatbot.png";
 
 const data_projects = [
   {
-    name: "Dev Portfolio",
-    image: portfolio,
-    deployed_url: "https://github.com/scantea/personal-react-portfolio",
-    github_url: "https://github.com/scantea/personal-react-portfolio",
+    name: "Mental Health Chatbot",
+    image: chatbot,
+    deployed_url: "https://github.com/scantea/openai_codex",
+    github_url: "https://github.com/scantea/openai_codex",
   },
   {
     name: "Dev Portfolio",
