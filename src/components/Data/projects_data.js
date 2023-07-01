5aef24b6e8d519d435012d0a0a9a8f87d4827264
scantea <71529653+scantea@users.noreply.github.com>
2023-07-01 02:02:11 -0700

    Added projects

--- src/components/Data/projects_data.js
+++ src/components/Data/projects_data.js
@@ -1,5 +1,7 @@
 import portfolio from "../../assets/ProjectImages/web-pic.png";
 import chatbot from "../../assets/ProjectImages/chatbot.png";
+import iphone from "../../assets/ProjectImages/iphone.png";
+import board from "../../assets/ProjectImages/board.png";
 
 const data_projects = [
   {
@@ -15,16 +17,16 @@ const data_projects = [
     github_url: "https://github.com/scantea/personal-react-portfolio",
   },
   {
-    name: "Dev Portfolio",
-    image: portfolio,
-    deployed_url: "https://github.com/scantea/personal-react-portfolio",
-    github_url: "https://github.com/scantea/personal-react-portfolio",
+    name: "Inspiration board",
+    image: board,
+    deployed_url: "https://mean-pugs-inspo-board-api.herokuapp.com/",
+    github_url:
+      "https://github.com/scantea/front-end-inspiration-board/tree/css",
   },
   {
-    name: "Dev Portfolio",
-    image: portfolio,
-    deployed_url: "https://github.com/scantea/personal-react-portfolio",
-    github_url: "https://github.com/scantea/personal-react-portfolio",
+    name: "Mental Health Journaling App",
+    image: iphone,
+    github_url: "https://github.com/lzhang39/WavesCapstoneRepo2",
   },
 ];
 
